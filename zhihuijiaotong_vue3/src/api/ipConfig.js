cc7244985699f50281fc7b32cc1f9ac3b914e111
DrMOUSE-commit <tedgamer@163.com>
2026-06-09 19:26:52 +0800

    Set local default API base URL

--- zhihuijiaotong_vue3/src/api/ipConfig.js
+++ zhihuijiaotong_vue3/src/api/ipConfig.js
@@ -1,4 +1,4 @@
-const productionUrl = import.meta.env.VITE_API_BASE_URL || ''
+const productionUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/IntelliDetect'
 const developmentUrl = ''
 const ip = import.meta.env.PROD ? productionUrl : developmentUrl
 
