d24e21088c3cac0e390a5a91cc4bd2bcfbfbdc58
DrMOUSE-commit <tedgamer@163.com>
2026-06-10 16:02:38 +0800

    Update Vite frontend configuration

--- zhihuijiaotong_vue3/vite.config.js
+++ zhihuijiaotong_vue3/vite.config.js
@@ -1,38 +1,35 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-
-export default ({mode}) => {
-    return defineConfig({
-        plugins: [
-            vue()],
-        server: {
-            port: 8230, // 新的端口号
-            proxy: {
-                '/api': {
-                    target: 'http://localhost:8080',
-                    changeOrigin: true,
-                    rewrite: (path) => `/IntelliDetect${path}`
-                }
-            }
+export default () => {
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      port: 8230,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => `/IntelliDetect${path}`,
         },
-        base: '/',
-        resolve: {
-            alias: {
-                '@': '/src',
-                // 其他别名
-            },
+      },
+    },
+    base: '/',
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+          silenceDeprecations: ['legacy-js-api'],
+          sassOptions: {
+            outputStyle: 'compressed',
+          },
         },
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern', // 使用新版API
-                    silenceDeprecations: ['legacy-js-api'], // 临时消除警告
-                    sassOptions: {
-                        outputStyle: 'compressed',
-                    },
-                },
-            },
-        }
-    });
+      },
+    },
+  });
 };
