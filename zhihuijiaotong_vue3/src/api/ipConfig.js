fb85b802c430fe293402eafe9b597b47089f2a25
DrMOUSE-commit <tedgamer@163.com>
2026-06-10 16:02:11 +0800

    Update frontend environment configuration

--- zhihuijiaotong_vue3/src/api/ipConfig.js
+++ zhihuijiaotong_vue3/src/api/ipConfig.js
@@ -1,4 +1,8 @@
-const productionUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/IntelliDetect'
+function normalizeBaseUrl(url) {
+  return (url || '').replace(/\/+$/, '')
+}
+
+const productionUrl = normalizeBaseUrl(import.meta.env.VITE_API_BASE_URL)
 const developmentUrl = ''
 const ip = import.meta.env.PROD ? productionUrl : developmentUrl
 
